e156aec9baf83cd779549a2f3c3edc434f1b1c99
Luciano Silva <lurodrisilva@hotmail.com>
2024-10-06 17:15:15 -0300

    adding quantity mean

--- book.go
+++ book.go
@@ -1,14 +1,15 @@
 package models
 
 type Book struct {
-	Id        string  `json:"ID"`
-	StockName string  `json:"StockName"`
-	BookType  byte    `json:"BookType"`
-	Position  uint    `json:"Position"`
-	Direction byte    `json:"Direction"`
-	Price     float64 `json:"Price"`
-	DateTime  string  `json:"DateTime"`
-	OrderId   string  `json:"OrderId"`
-	Quantity  uint    `json:"Quantity"`
-	OfferType byte    `json:"OfferType"`
+	Id           string  `json:"ID"`
+	StockName    string  `json:"StockName"`
+	BookType     byte    `json:"BookType"`
+	Position     uint    `json:"Position"`
+	Direction    byte    `json:"Direction"`
+	Price        float64 `json:"Price"`
+	DateTime     string  `json:"DateTime"`
+	OrderId      string  `json:"OrderId"`
+	Quantity     uint    `json:"Quantity"`
+	OfferType    byte    `json:"OfferType"`
+	QuantityMean uint    `json:"QuantityMean"`
 }
