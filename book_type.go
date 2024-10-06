fa455c02c0e40091b7ed7260522a583b1fe83722
Luciano Silva <lurodrisilva@hotmail.com>
2024-10-06 16:23:42 -0300

    adding booking types

---
+++ book_type.go
@@ -0,0 +1,16 @@
+package models
+
+const (
+	// Add book
+	BookAdd byte = 'A'
+	// Update book
+	BookUpdate byte = 'U'
+	// End of messages
+	BookEnd byte = 'E'
+	// Cancel book
+	BookCancel byte = 'D'
+	// Direction of the book in case Buy
+	BookBuy byte = 'A'
+	// Direction of the book in case Sell
+	BookSell byte = 'V'
+)
