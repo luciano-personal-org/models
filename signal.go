b6ea24ee0359500f3684ca814b6615e3f1182a05
Luciano Silva <lurodrisilva@hotmail.com>
2024-12-07 18:02:26 -0300

    adding OrderType into Signal

--- signal.go
+++ signal.go
@@ -8,7 +8,8 @@ type Signal struct {
 	PriceOrigin float64         `json:"PriceOrigin"`     // The price that originated the signal
 	PriceTarget float64         `json:"PriceTarget"`     // The price target for the signal
 	StopLoss    float64         `json:"StopLoss"`        // The price for the loss of the signal
-	Strength    float64         `json:"Strength"`        // 0 to 1
+	Strength    float64         `json:"Strength"`        // 0.0 to 1.0
+	OrderType   OrderType       `json:"OrderType"`       // The type of order
 }
 
 // SignalDirection is an enum-like type for signal direction.
@@ -22,4 +23,29 @@ const (
 // String returns the string representation of the SignalDirection.
 func (d SignalDirection) String() string {
 	return [...]string{"BUY", "SELL"}[d]
+}
+
+type OrderType int
+
+const (
+	LIMIT OrderType = iota
+	MARKET
+	STOP_LOSS
+	STOP_LOSS_LIMIT
+	TAKE_PROFIT
+	TAKE_PROFIT_LIMIT
+	LIMIT_MAKER
+)
+
+// String returns the string representation of the SignalDirection.
+func (d OrderType) String() string {
+	return [...]string{
+		"LIMIT",
+		"MARKET",
+		"STOP_LOSS",
+		"STOP_LOSS_LIMIT",
+		"TAKE_PROFIT",
+		"TAKE_PROFIT_LIMIT",
+		"LIMIT_MAKER",
+	}[d]
 }
