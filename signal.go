e39eebf87914c0af391a7f258482638843fa3f33
Luciano Silva <lurodrisilva@hotmail.com>
2024-12-07 20:29:59 -0300

    adding StopGain

--- signal.go
+++ signal.go
@@ -5,7 +5,7 @@ type Signal struct {
 	ID        string          `json:"ID"`              // Unique identifier for the signal
 	Candle    Candle          `json:"Candle"`          // The candle that generated the signal
 	Direction SignalDirection `json:"SignalDirection"` // BUY or SELL
-	Price     float64         `json:"Price"`           // The price target for the signal
+	StopGain  float64         `json:"StopGain"`        // The price target for the signal
 	StopLoss  float64         `json:"StopLoss"`        // The price for the loss of the signal
 	Strength  float64         `json:"Strength"`        // 0.0 to 1.0
 	OrderType OrderType       `json:"OrderType"`       // The type of order
