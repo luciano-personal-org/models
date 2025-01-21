9738c0adcfd603e5cad004908744d27ced1d6571
lurodrisilva <lurodrisilva@hotmail.com>
2025-01-21 20:52:00 -0300

    adding comission

--- booking.go
+++ booking.go
@@ -2,10 +2,11 @@ package models
 
 // Quote struct for the Order.
 type Booking struct {
-	ID              string          `json:"ID"`              // Unique identifier for the signal
-	Orders          Orders          `json:"Orders"`          // The candle that generated the signal
-	Quantity        float64         `json:"Quantity"`        // The quantity of the order
-	MarketDirection MarketDirection `json:"MarketDirection"` // The type of order
+	ID                     string          `json:"ID"`                     // Unique identifier for the signal
+	Orders                 Orders          `json:"Orders"`                 // The candle that generated the signal
+	Quantity               float64         `json:"Quantity"`               // The quantity of the order
+	QuantityAfterComission float64         `json:"QuantityAfterComission"` // The quantity of the order after comission
+	MarketDirection        MarketDirection `json:"MarketDirection"`        // The type of order
 }
 
 type MarketDirection int
